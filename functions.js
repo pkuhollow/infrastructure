7955c69e0deedbfe2d1aaf20a9e8ef4ca385a3a2
xmcp <xiaoyuanan2000@sina.com>
2019-08-23 13:49:21 +0800

    add dark mode

--- functions.js
+++ functions.js
@@ -3,3 +3,16 @@ export function get_json(res) {
     return res.json();
 }
 
+export function listen_darkmode(override) { // override: true/false/undefined
+    function update_color_scheme() {
+        if(override===undefined ? window.matchMedia('(prefers-color-scheme: dark)').matches : override)
+            document.body.classList.add('root-dark-mode');
+        else
+            document.body.classList.remove('root-dark-mode');
+    }
+
+    update_color_scheme();
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(()=>{
+        update_color_scheme();
+    });
+}
