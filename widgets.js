f1502d1e7b2df4a311543d25e7f484952e9d7191
xmcp <xiaoyuanan2000@sina.com>
2019-09-22 15:11:48 +0800

    add dynamic app loading

--- widgets.js
+++ widgets.js
@@ -13,6 +13,8 @@ import appicon_imasugu_rev from './appicon/imasugu_rev.png';
 import appicon_syllabus from './appicon/syllabus.png';
 import appicon_score from './appicon/score.png';
 import appicon_course_survey from './appicon/course_survey.png';
+import {PKUHELPER_ROOT} from './const';
+import {get_json} from './functions';
 
 function pad2(x) {
     return x<10 ? '0'+x : ''+x;
@@ -50,31 +52,88 @@ export function GlobalTitle(props) {
     );
 }
 
-const APPS=[
+const FALLBACK_APPS=[
     // id, text, url, icon_normal, icon_hover, new_tab
-    ['hole','树洞','/hole',appicon_hole,appicon_hole,false],
+    ['hole','树洞','/hole',appicon_hole,null,false],
     ['imasugu','教室','/spare_classroom',appicon_imasugu,appicon_imasugu_rev,false],
-    ['syllabus','课表','/syllabus',appicon_syllabus,appicon_syllabus,false],
-    ['score','成绩','/my_score',appicon_score,appicon_score,false],
+    ['syllabus','课表','/syllabus',appicon_syllabus,null,false],
+    ['score','成绩','/my_score',appicon_score,null,false],
     ['course_survey','测评','http://courses.pinzhixiaoyuan.com/',appicon_course_survey,appicon_course_survey,true],
 ];
-export function AppSwitcher(props) {
-    let cur_id=props.appid;
-    return (
-        <div className="app-switcher">
-            <span className="app-switcher-desc app-switcher-left">
-                <a href="/">PKUHelper</a>
-            </span>
-            {APPS.map(([id,title,url,icon_normal,icon_hover,new_tab])=>(
-                <a key={id} className={'app-switcher-item'+(id===cur_id ? ' app-switcher-item-current' : '')} href={url} target={new_tab ? '_blank' : '_self'}>
-                    <img src={icon_normal} className="app-switcher-logo-normal" />
-                    <img src={icon_hover} className="app-switcher-logo-hover" />
-                    {title}
-                </a>
-            ))}
-            <span className="app-switcher-desc  app-switcher-right">
-                网页版
-            </span>
-        </div>
-    );
+const SWITCHER_DATA_VER='switcher_1';
+const SWITCHER_DATA_URL=PKUHELPER_ROOT+'web_static/appswitcher_items.json';
+
+export class AppSwitcher extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            apps: this.get_apps_from_localstorage(),
+        }
+    }
+
+    get_apps_from_localstorage() {
+        let ret=FALLBACK_APPS;
+        if(localStorage['APPSWITCHER_ITEMS'])
+            try {
+                let content=JSON.parse(localStorage['APPSWITCHER_ITEMS'])[SWITCHER_DATA_VER];
+                if(!content || !content.length)
+                    throw new Error('content is empty');
+
+                ret=content;
+            } catch(e) {
+                console.error('load appswitcher items from localstorage failed');
+                console.trace(e);
+            }
+
+        return ret;
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            fetch(SWITCHER_DATA_URL)
+                .then((res)=>{
+                    if(!res.ok) throw Error(`网络错误 ${res.status} ${res.statusText}`);
+                    return res.text();
+                })
+                .then((txt)=>{
+                    if(txt!==localStorage['APPSWITCHER_ITEMS']) {
+                        console.log('loaded new appswitcher items',txt);
+                        localStorage['APPSWITCHER_ITEMS']=txt;
+
+                        this.setState({
+                            apps: this.get_apps_from_localstorage(),
+                        });
+                    } else {
+                        console.log('appswitcher items unchanged');
+                    }
+                })
+                .catch((e)=>{
+                    console.error('loading appswitcher items failed');
+                    console.trace(e);
+                });
+        },500);
+    }
+
+    render() {
+        let cur_id=this.props.appid;
+        return (
+            <div className="app-switcher">
+                <span className="app-switcher-desc app-switcher-left">
+                    <a href="/">PKUHelper</a>
+                </span>
+                {this.state.apps.map(([id,title,url,icon_normal,icon_hover,new_tab])=>(
+                    <a key={id} className={'app-switcher-item'+(id===cur_id ? ' app-switcher-item-current' : '')} href={url} target={new_tab ? '_blank' : '_self'}>
+                        {!!icon_normal && [
+                            <img key="normal" src={icon_normal} className="app-switcher-logo-normal" />,
+                            <img key="hover" src={icon_hover||icon_normal} className="app-switcher-logo-hover" />
+                        ]}
+                        <span>{title}</span>
+                    </a>
+                ))}
+                <span className="app-switcher-desc  app-switcher-right">
+                    网页版
+                </span>
+            </div>
+        );
+    }
 }
